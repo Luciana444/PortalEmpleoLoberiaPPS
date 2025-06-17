5a41ba33ec705de075759bc3aa11effed05ff35b
Nahu Caroseli <nahucaroseli2100@gmail.com>
2025-06-17 13:38:01 -0300

    agregado dependencia cors para middleware con express

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import {cors} from './src/configs/corsConfig.js';
-import { router } from './src/routes/router.js';
+import {router} from './src/routes/router.js';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
+
+
 app.use(express.json());
+
+
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:3000`);
 });
