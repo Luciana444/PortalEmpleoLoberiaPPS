dd737ea50997f027e9b872e69a67b6f9eb86de40
Santiago Lázaro <santi.lazaro@gmail.com>
2025-06-26 14:50:14 -0300

    empezada estructura para conectar con el back.

--- Frontend/portal-trabajo/src/app/landing/landing.component.ts
+++ Frontend/portal-trabajo/src/app/landing/landing.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -9,11 +10,27 @@ import { Router } from '@angular/router';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
+  
+  public getJson: any;
+
   navigateToLogin() {
     this.router.navigate(['/login']);
   }
+
   navigateToFormSelector() {
     this.router.navigate(['/form-selector']);
+  }
+
+  public getData() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(
+      //   (data) => {
+      //   this.getJson = data;
+      //   console.log(this.getJson);
+      // }, (error) => {
+      //   console.error('Error fetching data:', error);
+      // }
+    );
   }
 }
