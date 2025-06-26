d0905ed6635354975793c3f5319c6e412578032b
Santiago Lázaro <santi.lazaro@gmail.com>
2025-06-26 14:29:45 -0300

    Landing header versión mobile

--- Frontend/portal-trabajo/src/app/landing/landing.component.ts
+++ Frontend/portal-trabajo/src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-
+  constructor(private router: Router) {}
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+  navigateToFormSelector() {
+    this.router.navigate(['/form-selector']);
+  }
 }
