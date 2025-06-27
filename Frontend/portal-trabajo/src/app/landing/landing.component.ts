74862a1a9c5cde3041453dfb4db1a75a42d55c6b
Santiago Lázaro <santi.lazaro@gmail.com>
2025-06-27 16:42:14 -0300

    Landing dinamico terminado

--- Frontend/portal-trabajo/src/app/landing/landing.component.ts
+++ Frontend/portal-trabajo/src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { title } from 'node:process';
 
 
 @Component({
@@ -11,6 +12,27 @@ import { Router } from '@angular/router';
 export class LandingComponent {
   constructor(private router: Router, private http: HttpClient) {}
   
+  offers = [
+    { id: 1,
+      user: "Restaurante Lobería", 
+      location: "Lobería",
+      title: "Ayudante de Cocina",
+      description: "Renombrada Parrilla incorpora ayudante con experiencia. Se busca una persona proactiva, dinámica, con ganas de formar parte de un equipo de trabajo estable. PRINCIPALES        TAREAS: Realizar la mise en place. Preparar, cocinar y     despachar los pedidos. Preparar y cocinar la comida del personal.....",
+      modality: "Presencial",
+      date: "01/10/2023",
+      img:""
+    }, 
+    { id: 2,
+      user: "Restaurante Lobería", 
+      location: "Lobería",
+      title: "Ayudante de Cocina",
+      description: "Renombrada Parrilla incorpora ayudante con experiencia. Se busca una persona proactiva, dinámica, con ganas de formar parte de un equipo de trabajo estable. PRINCIPALES        TAREAS: Realizar la mise en place. Preparar, cocinar y     despachar los pedidos. Preparar y cocinar la comida del personal.....",
+      modality: "Presencial",
+      date: "01/10/2023",
+      img:""
+    }
+  ];
+
   public getJson: any;
 
   navigateToLogin() {
