2a998a5cccf8a6e2f4535df7cb587273b2c224e3
Nahu Caroseli <nahucaroseli2100@gmail.com>
2025-06-27 14:36:57 -0300

    corregido error de borrado de archivo para testing

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+// app.js
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './src/routes/authRoutes.js'; // Ajustá si tu ruta está en otra carpeta
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/auth', authRoutes);
+
+export default app;
