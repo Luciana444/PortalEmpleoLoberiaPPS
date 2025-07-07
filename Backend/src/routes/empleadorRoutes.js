5415f36acc1c206236b82a287e52b5ab569ea0ce
Nahu Caroseli <nahucaroseli2100@gmail.com>
2025-07-07 16:44:30 -0300

    agregado endpoint para obtener datos de una empresa especifica

--- Backend/src/routes/empleadorRoutes.js
+++ Backend/src/routes/empleadorRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { onlyEmpresa } from '../middlewares/onlyEmpresa.js';
-import { actualizarPerfilEmpresa } from '../controllers/empleadorController.js';
+import { actualizarPerfilEmpresa, obtenerDatosEmpresa } from '../controllers/empleadorController.js';
 
 export const empleadoRoutes = express.Router();
 
 
 empleadoRoutes.patch('/actualizar/perfil',authMiddleware,onlyEmpresa,actualizarPerfilEmpresa);
 
+empleadoRoutes.get('/datos',authMiddleware,onlyEmpresa,obtenerDatosEmpresa);
 
 
 export default empleadoRoutes;
