ae72b41e88d19790aed501bce82ac1d3ad77fe0e
Santiago Lázaro <santi.lazaro@gmail.com>
2025-07-08 16:30:31 -0300

    finalizado perfil empleado

--- Frontend/portal-trabajo/src/app/employee-profile/employee-profile.component.ts
+++ Frontend/portal-trabajo/src/app/employee-profile/employee-profile.component.ts
@@ -1,9 +1,10 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ProfileComponent } from '../profile/profile.component';
 import { AcademicBackgroundComponent } from '../academic-background/academic-background.component';
 import { AttatchCvComponent } from '../attatch-cv/attatch-cv.component';
+import { Employee } from '../../models/employee.model';
 
 @Component({
   selector: 'app-employee-profile',
@@ -11,6 +12,35 @@ import { AttatchCvComponent } from '../attatch-cv/attatch-cv.component';
   templateUrl: './employee-profile.component.html',
   styleUrl: './employee-profile.component.scss'
 })
-export class EmployeeProfileComponent {
-  name: string = "Juana Perez";
+export class EmployeeProfileComponent implements OnInit {
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  employee: Employee | null = null;
+
+  mockEmployee: Employee = {
+    id: 12345678,
+    name: 'Juan',
+    surname: 'Pérez',
+    location: 'Buenos Aires',
+    country: 'Argentina',
+    province: 'Buenos Aires',
+    birthdate: '1985-05-15',
+    cuil: 20123456789,
+    hasDisability: false,
+    phoneNumber: '+5491145678901',
+    email: 'juan.perez@example.com',
+    address: 'Av. Corrientes 1234',
+  };
+
+  loadData() {
+
+    this.employee = { ...this.mockEmployee };
+    console.log(this.employee.id);
+    console.log(this.mockEmployee.cuil);
+  }
 }
+
+
---
+++ Frontend/portal-trabajo/src/models/employee.model.ts
@@ -0,0 +1,15 @@
+export interface Employee {
+    name: string;
+    surname: string;
+    location: string;
+    country: string;
+    province: string;
+    birthdate: string;
+    id: number;
+    cuil: number;
+    hasDisability: boolean;
+
+    phoneNumber: string;
+    email: string;
+    address: string;
+}
