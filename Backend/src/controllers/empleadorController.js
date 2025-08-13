7c88e9397472c3a556c93a974547f43c6794b070
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2025-08-13 13:41:22 -0300

    corregido error con id en return

--- Backend/src/controllers/empleadorController.js
+++ Backend/src/controllers/empleadorController.js
@@ -233,14 +233,14 @@ export const crearOfertaLaboral = async (req,res)=>{
 
 
 
-    await crearOferta(id_empresa, req.body);
-
+    const nuevaOferta = await crearOferta(id_empresa, req.body);
     
     //res.status(200).json({ message: 'Oferta creada correctamente' });
     //agrego que devuelva el id de la oferta para el test:
-    res.status(200).json({ message: 'Oferta creada correctamente', id_oferta: nuevaOferta.id });
+   return res.status(200).json({ message: 'Oferta creada correctamente', id_oferta: nuevaOferta.id });
 
   } catch (error) {
+    console.log(error);
     res.status(500).json({message:'Error al crear oferta'})
   }
 };
